9a00bc6548ef385362e190c20e8777b896141a09
poorananb <pooranan@gmail.com>
2015-09-11 08:55:47 +0000

    Created event controller

---
+++ app/controllers/api/v1/events_controller.rb
@@ -0,0 +1,87 @@
+class Api::V1::EventsController < ApplicationController
+    
+  def index
+    logger.debug 'Hit Events index'
+    @events = Event.all
+    respond_with @events
+  end
+
+  def new
+  end
+  
+  def create
+    respond_to do |format|
+      if Event.create(event_params)
+        format.json do
+          render :json => {
+            :status => :ok,
+            :message => 'Event was created Successfully!'
+          }.to_json
+        end      
+      else    
+        format.json do
+          render :json => {
+            :status => :error,
+            :message => @event.errors
+          }.to_json
+        end  
+      end
+    end  
+  end
+  
+  def show
+    respond_with Event.find(params[:id])
+  end
+  
+  def edit
+    respond_with Event.find(params[:id])
+  end
+  
+  def update
+    @event = Event.find(params[:id])
+    respond_to do |format|
+      if @event.update(event_params)
+        format.json do
+          render :json => { 
+             :status => :ok, 
+             :message => "Event was successfully updated.!"
+          }.to_json
+        end  
+      else
+        format.json do 
+          render :json => {
+            :message => @event.errors, 
+            :status => :error #unprocessable_entity 
+          }.to_json
+        end
+      end 
+    end
+  end
+
+  def destroy
+    @event = Event.find(params[:id])
+    respond_to do |format|
+      if @event.destroy
+        format.json do
+          render :json => { 
+             :status => :ok, 
+             :message => "Event was successfully deleted.!"
+          }.to_json
+        end  
+      else
+        format.json do 
+          render :json => {
+            :message => @event.errors, 
+            :status => :error #unprocessable_entity 
+          }.to_json
+        end
+      end 
+    end
+  end
+  
+  
+  private
+  def event_params
+    params.require(:event).permit(:praxis_code, :event_id, :occurred_at, :zipcode, :country) if params[:event]
+  end
+end
