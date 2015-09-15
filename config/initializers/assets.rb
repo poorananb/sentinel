e3d8c866a078d8c5e7ade4ede72e2629fb055bc5
parjapat <roshanparjapat@gmail.com>
2015-09-15 16:12:26 +0000

    graph

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -7,5 +7,6 @@ Rails.application.config.assets.version = '1.0'
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
 
-Rails.application.config.assets.precompile += %w( chart.js )
+
+Rails.application.config.assets.precompile += %w( radial-progress-chart.js )
 Rails.application.config.assets.precompile += %w( font-awesome.css )
