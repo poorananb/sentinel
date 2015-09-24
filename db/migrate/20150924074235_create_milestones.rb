17a576d17369e75bd10b8807ae16ba391d7ba7f8
poorananb <pooranan@gmail.com>
2015-09-24 07:47:02 +0000

    Milestones addede

---
+++ db/migrate/20150924074235_create_milestones.rb
@@ -0,0 +1,11 @@
+class CreateMilestones < ActiveRecord::Migration
+  def change
+    create_table :milestones do |t|
+      t.string :org_code
+      t.string :client_code
+      t.string :key
+
+      t.timestamps null: false
+    end
+  end
+end
