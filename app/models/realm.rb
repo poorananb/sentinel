c489426d46dc9ce9f8afa5f6a30a798369b02b10
poorananb <pooranan@gmail.com>
2015-12-05 07:00:43 +0000

    Removed client code from Indices

--- app/models/realm.rb
+++ app/models/realm.rb
@@ -1,4 +1,4 @@
 class Realm < ActiveRecord::Base
     validates :name, presence: true 
-    validates :code, presence: true, length: { in: 3..10 }, uniqueness: { case_sensitive: false }
+    validates :code, presence: true, length: { in: 2..10 }, uniqueness: { case_sensitive: false }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -16,6 +16,7 @@ ActiveRecord::Schema.define(version: 20151205041443) do
   create_table "clients", force: :cascade do |t|
     t.string   "code",       limit: 10
     t.string   "name"
+    t.string   "org_code"
     t.datetime "created_at",            null: false
     t.datetime "updated_at",            null: false
     t.string   "timezone"
@@ -71,7 +72,9 @@ ActiveRecord::Schema.define(version: 20151205041443) do
     t.string   "org_code"
     t.string   "client_code"
     t.string   "key"
-    t.string   "label"
+    t.string   "label_name"
+    t.string   "realm_code"
+    t.string   "icon"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
   end
@@ -80,6 +83,7 @@ ActiveRecord::Schema.define(version: 20151205041443) do
     t.string   "org_code"
     t.string   "client_code"
     t.string   "key"
+    t.string   "realm_code"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
   end
@@ -97,6 +101,7 @@ ActiveRecord::Schema.define(version: 20151205041443) do
     t.string   "praxis_code"
     t.string   "org_code"
     t.string   "client_code"
+    t.string   "milestone_key"
     t.string   "realm_code"
     t.string   "prosess_code"
     t.string   "stage_code"
