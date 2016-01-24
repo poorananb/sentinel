2c6d6994c3095bd10cd14873a0712e844c54a53c
nagarajsp <nagaraj@specommerce.com>
2016-01-24 09:51:51 +0000

    cron parser

--- app/mailers/job_mailer.rb
+++ app/mailers/job_mailer.rb
@@ -1,16 +1,38 @@
 # file: app/mailers/job_mailer.rb
 class JobMailer < ActionMailer::Base
+  require 'date'
+  
   def check_job_running_status(options)
     @indices = Index.all
     
     @indices.each do |index|
         @cron = ''
         @cron = index.cron
-        
+        @jobkey = index.jobkey
+        @mins = index.run_length
         cron_parser = CronParser.new(@cron)
-        next_comming_time = cron_parser.next(Time.now.in_time_zone(Time.zone.name))
+        @last_cron = cron_parser.last(Time.now) #.in_time_zone(Time.zone.name)
+        @next_cron = cron_parser.next(Time.now.in_time_zone(Time.zone.name))
         
-        Rails.logger.debug("My password: #{next_comming_time}")
+        @runs = Run.where(["jobkey = ? and created_at = ?, value != ?", @jobkey, @last_cron, 'done']).limit(1)
+        
+        if(!@runs.blank?)
+          @runs.each do |run|
+            created_at = run.created_at
+            
+            created_at2 = created_at.in_time_zone(Time.zone.name)
+            minutes = (Time.now - created_at2) + 10
+            
+            if(minutes > @mins)
+              #send mail
+            end
+            
+            Rails.logger.debug("My password: #{@last_cron}")
+          end
+        else
+          run = Run.new(jobkey: @jobkey, stage: 'mia', value: 'missing_in_action')
+          run.save()
+        end
     end
   end
 end
